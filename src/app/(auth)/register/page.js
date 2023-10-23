57fe8f03deaa153a2d2ce863850d057096ff8a5f
Kawsar <web.kawsarahmed@gmail.com>
2023-10-23 09:05:49 +0600

    added login, register page design

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,43 +1,46 @@
-"use client"
+"use client";
 
 import axios from "axios";
 import { toast } from "react-toastify";
 
-
-
-
-
 const Register = () => {
-
   const handleSubmit = async () => {
     try {
+      const result = await axios.post(
+        `${process.env.NEXT_PUBLIC_STRAPI_SERVER_URL}/api/auth/local/register`,
+        {
+          username: "webkawsar123",
+          email: "web.kawsarahmed123@gmail.com",
+          password: "abc123",
+        }
+      );
 
-      const result = await axios.post(`${process.env.NEXT_PUBLIC_STRAPI_SERVER_URL}/api/auth/local/register`, {
-        username: 'webkawsar123',
-        email: 'web.kawsarahmed123@gmail.com',
-        password: 'abc123',
-      })
-  
-      console.log(result, 'result')
+      console.log(result, "result");
 
       // show success message
       toast.success("Registration successful!");
-
     } catch (error) {
-
-      console.log(error?.response?.data?.error, 'error')
+      console.log(error?.response?.data?.error, "error");
 
       // show error message
-      toast.error(error?.response?.data?.error?.message ?? 'Something went wrong!');
+      toast.error(
+        error?.response?.data?.error?.message ?? "Something went wrong!"
+      );
     }
-  }
+  };
 
   return (
     <div className="p-10">
-      <p>Please register</p>
-      <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Register</button>
-    </div>
-  )
-}
+      <p> Please register</p>
 
-export default Register
+      <button
+        onClick={handleSubmit}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Register
+      </button>
+    </div>
+  );
+};
+
+export default Register;
--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -2,85 +2,79 @@ import axios from "axios";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 
-const SERVER_API_URL = process.env.NEXT_PUBLIC_STRAPI_SERVER_URL || 'http://127.0.0.1:1337'
+const SERVER_API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_SERVER_URL || "http://127.0.0.1:1337";
 
 export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-      CredentialsProvider({
-        name: 'Email',
-        credentials: {
-          username: {
-            label: 'Username',
-            type: 'text',
-            placeholder: 'username'
-          },
-          password: {
-            label: 'Password',
-            type: 'password',
-            placeholder: 'password'
-          }
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username",
         },
-        async authorize(credentials) {
-
-          // database query for checking strapi user exists or not
-          try {
-
-            const { data } = await axios.post(`${SERVER_API_URL}/api/auth/local`, {
-              identifier: credentials.identifier,
-              password: credentials.password
-            })
-
-            return data;
-            
-          } catch (error) {
-
-            return null;
-          }
-
-        }
-      }),
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-        profile(githubProfile) {
-
-          // console.log(githubProfile, 'githubProfile')
-          // save into database
-
-          return {
-            ...githubProfile,
-            image: githubProfile.avatar_url,
-            id: githubProfile.id.toString(),
-            role: githubProfile.role ?? 'user'
-          }
-        }
-      })
-
-      // ...add more providers here
-    ],
-    callbacks: {
-      async jwt({ token, user, account, profile, isNewUser }) {
-
-        const isSignIn = user ? true : false;
-        if (isSignIn) {
-          
-          token.id = user.user.id;
-          token.jwt = user.jwt;
-          token.user = user.user;
-        }
-
-        return Promise.resolve(token);
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
       },
-      async session({ session, token, user }) {
+      async authorize(credentials) {
+        // database query for checking strapi user exists or not
+        try {
+          const { data } = await axios.post(
+            `${SERVER_API_URL}/api/auth/local`,
+            {
+              identifier: credentials.identifier,
+              password: credentials.password,
+            }
+          );
 
-        // modify for front-end
-        session.id = token.id;
-        session.accessToken = token.accessToken;
-        session.user = token.user;
-      
-        return Promise.resolve(session);
+          return data;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      profile(githubProfile) {
+        console.log(githubProfile, "githubProfile");
+        // save into database
+
+        return {
+          ...githubProfile,
+          image: githubProfile.avatar_url,
+          id: githubProfile.id.toString(),
+          role: githubProfile.role ?? "user",
+        };
+      },
+    }),
+
+    // ...add more providers here
+  ],
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) {
+      const isSignIn = user ? true : false;
+      if (isSignIn) {
+        token.id = user.user.id;
+        token.jwt = user.jwt;
+        token.user = user.user;
       }
-    }
-}
 
+      return Promise.resolve(token);
+    },
+    async session({ session, token, user }) {
+      // modify for front-end
+      session.id = token.id;
+      session.accessToken = token.accessToken;
+      session.user = token.user;
+
+      return Promise.resolve(session);
+    },
+  },
+};
